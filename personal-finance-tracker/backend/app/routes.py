7f0443e65e5b34a39be44d4755d8e4255e2419d0
mddamanalam <alammdaman2019@gmail.com>
2025-04-29 00:07:50 +0530

    "Change on Project Architecture and File Structure"

--- personal-finance-tracker/backend/app/routes.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, jsonify
-
-main = Blueprint('main', __name__)
-
-@main.route('/api', methods=['GET'])
-def api_home():
-    return jsonify({"message": "Welcome to the Personal Finance Tracker API!"})
